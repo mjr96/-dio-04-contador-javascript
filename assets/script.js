f2cf2518a1fd2fa0469179deedc29a891e2cd6f9
Marcio Mariano de Oliveira Junior <marciojunior96@gmail.com>
2022-02-04 13:48:17 -0300

    Funcionalidades criadas no script.js e sistema esta funcionando 100%

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
-function increment(){
+var numeroAtual = 0;
+var numeroAtualTela = document.getElementById("numeroAtual");
 
+
+function increment(){
+    numeroAtual = numeroAtual + 1;
+    numeroAtualTela.innerHTML = numeroAtual;
 }
 
 function decrement(){
-
+    numeroAtual = numeroAtual - 1;
+    numeroAtualTela.innerHTML = numeroAtual;
 }
