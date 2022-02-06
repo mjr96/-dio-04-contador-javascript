da396bda94693fd7190e0e39cc8e573f09a5fa68
Marcio Mariano de Oliveira Junior <marciojunior96@gmail.com>
2022-02-06 17:10:24 -0300

    Modificacoes ncessarias feitas para adicionar o addEventListener click

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//criacao das funcoes de incremento e decremento 
 var numeroAtual = 0;
 var numeroAtualTela = document.getElementById("numeroAtual");
 
@@ -11,3 +12,14 @@ function decrement(){
     numeroAtual = numeroAtual - 1;
     numeroAtualTela.innerHTML = numeroAtual;
 }
+
+//Adicionando addEventListener
+var botaoDuvida = document.querySelector(".botaoDuvida");
+var conteudoNovo = document.querySelector(".conteudoNovo");
+
+
+function addConteudo(){
+    conteudoNovo.innerHTML = "------------Clique um dos botoes abaixo------------";
+}
+
+botaoDuvida.addEventListener("click", addConteudo);
